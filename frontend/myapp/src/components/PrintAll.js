415b7cfc492c768fa58cfdca14310b8d3bc37bcb
BaldurLeDur <robin.frouin.04@gmail.com>
2022-12-13 20:02:07 +0100

    j'adore la keu

--- frontend/myapp/src/components/PrintAll.js
+++ frontend/myapp/src/components/PrintAll.js
@@ -2,11 +2,27 @@ import { useEffect, useState } from "react";
 import AddToPokedex from "../api/AddToPokedex";
 import { getAll } from "../api/Fetch";
 import { Button } from 'react-bootstrap';
-
-
+import Alert from 'react-bootstrap/Alert';
 
 function PrintAll(){
     const [ pokemons, setPokemons ] = useState([]);
+
+
+function random(pokemon){
+    const chance = pokemon.chance;
+    const foo = Math.random() * 100;
+    if (foo < chance){
+        AddToPokedex(pokemon);
+        <Alert key="sucess" variant="sucess">
+          Captured !
+        </Alert>;
+    }else{
+        <Alert key="danger" variant="danger">
+            failed !
+        </Alert>;
+    }
+    
+}
     
 
     useEffect(() => {
@@ -20,9 +36,10 @@ function PrintAll(){
         {
             pokemons.map((pokemon,key) =>{
                 return <div key={key} className="bloc-pokemon">
-                    <img src={pokemon.image} />
-                    <h2>{pokemon.name}</h2>
-                    <Button variant ="success" onClick={()=>AddToPokedex(pokemon)}>Capturer !</Button>
+                        <img src={pokemon.image} />
+                        <h2>{pokemon.name}</h2>
+                        <Button variant ="success" onClick={random(pokemon)}>Capturer !</Button>
+                        
                 </div>
             })
         }
