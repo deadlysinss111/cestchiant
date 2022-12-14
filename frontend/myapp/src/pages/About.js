0104aa3dbf9c0db2e2a9fd59a7f85e41e7b237a7
BaldurLeDur <robin.froui.04@gmail.com>
2022-12-14 18:12:17 +0100

    ca marche et c'est beau

--- frontend/myapp/src/pages/About.js
+++ frontend/myapp/src/pages/About.js
@@ -2,8 +2,10 @@ import Menu from "../components/Menu";
 function About(props){
     return <><div>
         <Menu />
-        <h1>A propos !</h1>
-        <p>we have nothing to say but you won't be able to tell it to anyone.</p>
+        <div id="rule">
+            <h1>A propos !</h1>
+            <p>we have nothing to say but you won't be able to tell it to anyone.</p>
+        </div>
     </div>
     <footer id="foot" className="hidden">AntoineLeBG</footer>
     </>
